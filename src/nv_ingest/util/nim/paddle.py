01b02236c63fd99b793a8b29857df1c9960c88ff
edknv <edwardk@nvidia.com>
2024-12-02 13:19:58 -0800

    http - use new ga api for newer versions of paddle

--- src/nv_ingest/util/nim/paddle.py
+++ src/nv_ingest/util/nim/paddle.py
@@ -14,24 +14,24 @@ class PaddleOCRModelInterface(ModelInterface):
 
     def prepare_data_for_inference(self, data):
         # Expecting base64_image in data
-        base64_image = data['base64_image']
+        base64_image = data["base64_image"]
         image_array = base64_to_numpy(base64_image)
-        data['image_array'] = image_array
+        data["image_array"] = image_array
         return data
 
     def format_input(self, data, protocol: str, **kwargs):
-        image_data = data['image_array']
-        if protocol == 'grpc':
+        image_data = data["image_array"]
+        if protocol == "grpc":
             logger.debug("Formatting input for gRPC PaddleOCR model")
             image_data = preprocess_image_for_paddle(image_data, self.paddle_version)
             image_data = image_data.astype(np.float32)
             image_data = np.expand_dims(image_data, axis=0)
 
             return image_data
-        elif protocol == 'http':
+        elif protocol == "http":
             logger.debug("Formatting input for HTTP PaddleOCR model")
             # For HTTP, preprocessing is not necessary
-            base64_img = data['base64_image']
+            base64_img = data["base64_image"]
             payload = self._prepare_paddle_payload(base64_img)
 
             return payload
@@ -39,22 +39,22 @@ class PaddleOCRModelInterface(ModelInterface):
             raise ValueError("Invalid protocol specified. Must be 'grpc' or 'http'.")
 
     def parse_output(self, response, protocol: str):
-        if protocol == 'grpc':
+        if protocol == "grpc":
             logger.debug("Parsing output from gRPC PaddleOCR model")
             # Convert bytes output to string
             if isinstance(response, np.ndarray):
                 if response.dtype.type is np.object_ or response.dtype.type is np.bytes_:
                     output_strings = [
-                        output[0].decode('utf-8') if isinstance(output, bytes) else str(output) for output in response
+                        output[0].decode("utf-8") if isinstance(output, bytes) else str(output) for output in response
                     ]
                     return " ".join(output_strings)
                 else:
                     output_bytes = response.tobytes()
-                    output_str = output_bytes.decode('utf-8')
+                    output_str = output_bytes.decode("utf-8")
                     return output_str
             else:
                 raise ValueError("Unexpected response format: response is not a NumPy array.")
-        elif protocol == 'http':
+        elif protocol == "http":
             logger.debug("Parsing output from HTTP PaddleOCR model")
             return self._extract_content_from_paddle_response(response)
         else:
@@ -64,12 +64,21 @@ class PaddleOCRModelInterface(ModelInterface):
         # For PaddleOCR, the output is the table content as a string
         return output
 
+    def _is_version_early_access_legacy_api(self):
+        return self.paddle_version and (
+            packaging.version.parse(self.paddle_version) < packaging.version.parse("0.2.1-rc2")
+        )
+
     def _prepare_paddle_payload(self, base64_img: str) -> Dict[str, Any]:
         image_url = f"data:image/png;base64,{base64_img}"
-        image = {"type": "image_url", "image_url": {"url": image_url}}
 
-        message = {"content": [image]}
-        payload = {"messages": [message]}
+        if self._is_version_early_access_legacy_api():
+            image = {"type": "image_url", "image_url": {"url": image_url}}
+            message = {"content": [image]}
+            payload = {"messages": [message]}
+        else:
+            image = {"type": "image_url", "url": image_url}
+            payload = {"input": [image]}
 
         return payload
 
@@ -77,4 +86,11 @@ class PaddleOCRModelInterface(ModelInterface):
         if "data" not in json_response or not json_response["data"]:
             raise RuntimeError("Unexpected response format: 'data' key is missing or empty.")
 
-        return json_response["data"][0]["content"]
+        if self._is_version_early_access_legacy_api():
+            content = json_response["data"][0]["content"]
+        else:
+            text_detections = json_response["data"][0]["text_detections"]
+            # "simple" format
+            content = " ".join(t["text_prediction"]["text"] for t in text_detections)
+
+        return content
